c868cd167015db9a9d22b342630cd0a26f98770a
Pariring <paring@pikokr.dev>
2023-02-10 20:58:42 +0900

    feat: setup timeline route

--- src/components/MkRouter.tsx
+++ src/components/MkRouter.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-native'
 import { NotFoundPage } from '../views/NotFound'
+import { HomeLayout } from './layout/HomeLayout'
+import { TimelineView } from '../views/home/timeline/Timeline'
 
 export const MkRouter: React.FC = () => {
   return (
     <Routes>
+      <Route element={<HomeLayout />}>
+        <Route index element={<TimelineView />} />
+      </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { Outlet } from 'react-router-native'
 
-export const Layout = () => {
+export const HomeLayout = () => {
   return (
     <View>
       <Text>Test</Text>
+      <Outlet />
     </View>
   )
 }
