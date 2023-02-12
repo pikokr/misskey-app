97d86006360dc19f27850e0a1a2d8d689d152a7d
Pariring <paring@pikokr.dev>
2023-02-12 13:25:23 +0900

    feat: note type

---
+++ src/types/note.ts
@@ -0,0 +1,54 @@
+import { OnlineStatus } from './user'
+
+export enum NoteVisibility {
+  Public = 'public',
+  Home = 'home',
+  Followers = 'followers',
+  Specified = 'specified',
+}
+
+export interface Note {
+  id: string
+  createdAt: string
+  userId: string
+  user: {
+    id: string
+    name: string | null
+    username: string
+    host: string
+    avatarUrl: string
+    avatarBlurhash: string
+    isBot: boolean
+    isCat: boolean
+    instance: {
+      name: string
+      softwareName: string
+      softwareVersion: string
+      iconUrl: string
+      faviconUrl: string
+      themeColor: string
+    }
+    emojis: Record<string, string>
+    onlineStatus: OnlineStatus
+  }
+  text: string
+  cw: string | null
+  visibility: NoteVisibility
+  localOnly: boolean
+  renoteCount: number
+  repliesCount: number
+  // TODO
+  reactions: {}
+  // TODO
+  reactionEmojis: {}
+  // TODO
+  emojis: {}
+  // TODO
+  fileIds: []
+  // TODO
+  files: string
+  replyId: string | null
+  renoteId: string | string
+  uri: string
+  url: string
+}
