cada2485fcc5c31c05e12f1904a421b9e0492e18
Pariring <paring@pikokr.dev>
2023-02-12 22:51:56 +0900

    feat: load instance emojis on startup

---
+++ src/types/emoji.tsx
@@ -0,0 +1,6 @@
+export interface CustomEmoji {
+  name: string
+  aliases: string[]
+  category: string
+  url: string
+}
--- src/utils/accounts.ts
+++ src/utils/accounts.ts
@@ -2,12 +2,16 @@ import { atom } from 'nanostores'
 import * as Keychain from 'react-native-keychain'
 import React from 'react'
 import Axios, { AxiosInstance } from 'axios'
+import axios from 'axios'
+import { CustomEmoji } from '../types/emoji'
 
 export interface Account {
   host: string
   username: string
   token: string
   api: AxiosInstance
+  emojis: Record<string, CustomEmoji>
+  emojisByCategory: Record<string, CustomEmoji[]>
 }
 
 export const accountStore = atom<Record<string, Account>>({})
@@ -28,6 +32,42 @@ const createAxiosClient = (host: string, token: string): AxiosInstance => {
   return api
 }
 
+const fetchEmojis = async (
+  host: string,
+): Promise<[Record<string, CustomEmoji>, Record<string, CustomEmoji[]>]> => {
+  const {
+    data: { emojis },
+  } = await axios.get<{
+    emojis: CustomEmoji[]
+  }>(`https://${host}/api/emojis`)
+
+  const result: Record<string, CustomEmoji> = {}
+
+  const categoryResult: Record<string, CustomEmoji[]> = {}
+
+  for (const item of emojis) {
+    result[item.name] = item
+
+    for (const alias of item.aliases) {
+      if (!alias) {
+        continue
+      }
+      result[alias] = item
+    }
+
+    let items = categoryResult[item.category]
+
+    if (!items) {
+      items = []
+      categoryResult[item.category] = items
+    }
+
+    items.push(item)
+  }
+
+  return [result, categoryResult]
+}
+
 export const loadAccounts = async () => {
   // accountStore.set(JSON.parse(data))
 
@@ -45,11 +85,15 @@ export const loadAccounts = async () => {
 
     const host = creds.service.split('@')[1]
 
+    const [emojis, emojisByCategory] = await fetchEmojis(host)
+
     result[service] = {
       host,
       token: creds.password,
       username: creds.username,
       api: createAxiosClient(host, creds.password),
+      emojis,
+      emojisByCategory,
     }
   }
 
@@ -73,6 +117,8 @@ export const addAccount = async (
 
   const items = accountStore.get()
 
+  const [emojis, emojisByCategory] = await fetchEmojis(server)
+
   accountStore.set({
     ...items,
     [`${username}@${server}`]: {
@@ -80,6 +126,8 @@ export const addAccount = async (
       token,
       username: username,
       api: createAxiosClient(server, token),
+      emojis,
+      emojisByCategory,
     },
   })
 }
