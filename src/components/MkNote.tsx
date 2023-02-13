b183084a470986e50e4c5f5077afa14d8b5421e2
Pariring <paring@pikokr.dev>
2023-02-13 12:13:31 +0900

    feat: cw button

--- src/components/MkNote.tsx
+++ src/components/MkNote.tsx
@@ -6,6 +6,7 @@ import { MkAvatar } from './MkAvatar'
 import { Account } from '../utils/accounts'
 import { MkUserName } from './mfm/MkUserName'
 import { MfmRenderer } from './mfm/MfmRenderer'
+import { useTranslation } from 'react-i18next'
 
 const Components = {
   Container: styled.View<{ divider: boolean }>`
@@ -37,6 +38,24 @@ const Components = {
     color: ${({ theme }) =>
       theme.fg.replace('rgb', 'rgba').replace(')', ', 0.5)')}; // Opacity
   `,
+  CwContainer: styled.View`
+    height: 12px;
+  `,
+  CwButtonContainer: styled.View`
+    background-color: ${({ theme }) => theme.cwBg};
+    height: 15px;
+    padding: 0 6px;
+    justify-content: center;
+    top: 0;
+    position: absolute;
+    border-radius: 4px;
+  `,
+  CwTouchable: styled.TouchableNativeFeedback``,
+  CwButtonText: styled.Text`
+    color: ${({ theme }) => theme.cwFg};
+    font-size: 12px;
+    line-height: 15px;
+  `,
 }
 
 type Props = {
@@ -45,17 +64,47 @@ type Props = {
   account: Account
 }
 
-export class MkNote extends React.PureComponent<Props> {
+const CwButton: React.FC<{
+  onOpen: () => void
+  characterCount: number
+  open: boolean
+}> = ({ onOpen, characterCount, open }) => {
+  const { t } = useTranslation()
+
+  return (
+    <Components.CwContainer>
+      <Components.CwButtonContainer>
+        <Components.CwTouchable onPress={onOpen}>
+          <Components.CwButtonText>
+            {open
+              ? t('note.cw.close')
+              : t('note.cw.open', {
+                  characterCount,
+                })}
+          </Components.CwButtonText>
+        </Components.CwTouchable>
+      </Components.CwButtonContainer>
+    </Components.CwContainer>
+  )
+}
+
+export class MkNote extends React.PureComponent<Props, { cwOpen: boolean }> {
   constructor(props: Props) {
     super(props)
 
     this.updateMfmText()
+
+    this.state = { cwOpen: false }
   }
 
   componentDidUpdate(prevProps: Props) {
     if (prevProps.note.text !== this.props.note.text) {
       this.updateMfmText()
     }
+  }
+
+  openCW = () => {
+    this.setState({ cwOpen: !this.state.cwOpen })
   }
 
   updateMfmText() {
@@ -66,6 +115,20 @@ export class MkNote extends React.PureComponent<Props> {
 
   render() {
     const { note, divider = false, account } = this.props
+
+    const content = (
+      <>
+        {note.text && (
+          <Text>
+            <MfmRenderer
+              fontSize={14}
+              content={note.text.trim()}
+              emojis={note.emojis}
+            />
+          </Text>
+        )}
+      </>
+    )
 
     return (
       <Components.Container divider={divider}>
@@ -84,15 +147,19 @@ export class MkNote extends React.PureComponent<Props> {
               </Components.HandleHost>
             </Components.HandleContainer>
           </Components.UsernameContainer>
-          {note.text && (
-            <Text>
-              <MfmRenderer
-                fontSize={14}
-                content={note.text.trim()}
-                emojis={note.emojis}
-              />
-            </Text>
+          {note.cw && (
+            <>
+              <Text>
+                <MfmRenderer content={note.cw} emojis={note.emojis} />
+                <CwButton
+                  open={this.state.cwOpen}
+                  onOpen={this.openCW}
+                  characterCount={note.text?.length ?? 0}
+                />
+              </Text>
+            </>
           )}
+          {note.cw ? (this.state.cwOpen ? content : null) : content}
         </Components.Content>
       </Components.Container>
     )
